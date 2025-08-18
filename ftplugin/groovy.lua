7d4399e6f46b62c8e224ba8da39e7078961a6826
awells10 <awells10@chewy.com>
2025-08-18 08:21:45 -0700

    Spacing config for groovy files

---
+++ ftplugin/groovy.lua
@@ -0,0 +1,5 @@
+-- Set indentation for Groovy/Jenkins files
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.bo.expandtab = true -- use spaces instead of tabs
+vim.bo.softtabstop = 4 -- spaces when pressing tab
